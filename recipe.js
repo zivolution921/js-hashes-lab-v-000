3025637ce559404d39a9531eb3f3fb3755aad564
Ziv Zamechek <ziv921@yahoo.com>
2016-05-30 11:17:15 -0400

    Done.

--- recipe.js
+++ recipe.js
@@ -1,1 +1,22 @@
 'use strict';
+
+function addIngredient(recipe, ingredient, amount) {
+  recipe[ingredient] = amount;
+  return recipe;
+}
+
+function removeIngredient(recipe, ingredient) {
+  delete recipe[ingredient];
+  return recipe;
+}
+
+function updateIngredient(recipe, ingredient, amount) {
+  recipe[ingredient] = amount;
+  return recipe;
+}
+
+function readRecipe(recipe) {
+  for (var ingredient in recipe) {
+    console.log("this recipe calls for " + recipe[ingredient] + " of " + ingredient);
+  }
+}
